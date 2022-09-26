a103653231e9f89c77e0892bd2b1d96398f08b54
Henry Morrin <henry.morrin@iii.com>
2022-09-26 11:27:23 +0100

    Added jenkins and test file

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World!\n'
  
 # main driver function
 if __name__ == '__main__':
